4ca71e6fde0d8bfeb2a8e2a2d5033e7d8ac8b690
janb96 <janekb1996@gmail.com>
2019-11-17 09:41:35 +0100

    [NF] Dodanie nowych endpointow w serwisie products

--- products-microservice/App/routes/api/products.js
+++ products-microservice/App/routes/api/products.js
@@ -14,4 +14,49 @@ router.get('/', function(req, res, next) {
     );
 });
 
+router.get('/byProductID/:productID', (req, res, next) => {
+
+    const productID = req.params.productID;
+
+    products.findAll({
+        where: {
+            productID: productID
+        }
+    }).then(
+        result => res.send(result)
+    );
+});
+
+router.get('/byCategoryID/:categoryID', (req, res, next) => {
+
+    const categoryID = req.params.categoryID;
+
+    products.findAll({
+        where: {
+            categoryID: categoryID
+        }
+    }).then(
+        result => res.send(result)
+    );
+});
+
+router.post('/', (req, res, next) => {
+    const name = req.body.productName;
+    const price = req.body.productPrice;
+    const categoryID = req.body.categoryID;
+    const productURL = req.body.productURL;
+
+    const product = {
+        categoryID: categoryID,
+        productName: name,
+        productPrice: price,
+        productURL: productURL
+    };
+
+    products.create(product).then(
+        result => res.send(result)
+    );
+
+});
+
 module.exports = router;
