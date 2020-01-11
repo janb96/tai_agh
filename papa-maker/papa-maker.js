b2b3c0768220fd220fd80c2128e14a515e24c0f3
janb96 <janekb1996@gmail.com>
2020-01-11 21:06:13 +0100

    [PC] Remove unused code

--- papa-maker/papa-maker.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send("I'm making PAPAPAPAPAPAPPA"))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
